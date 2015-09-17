1df3ed5c61f98bebb0c5b173335311d9d4d19812
johnny pinheiro <pinheiro_dom@hotmail.com.br>
2015-09-17 01:21:29 -0300

    coloquei a primeira movimentação com o cenário parado, troquei a textura da prancha para uma de madeira.

---
+++ neve/Assets/Script/movePersonagem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePersonagem : MonoBehaviour {
+
+	public Rigidbody rigd;
+	public float speed;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+	 rigd = GetComponent<Rigidbody>();
+	 animator = GetComponentInChildren<Animator>();
+	 //state = "stop";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D)){
+			rigd.velocity = new Vector3(-speed, 0,0);
+			animator.SetBool ("right", true);
+		}else if(Input.GetKey(KeyCode.A)){
+			rigd.velocity = new Vector3(speed, 0,0);
+		}else{
+			animator.SetBool ("right", false);
+		}
+
+	}
+}
