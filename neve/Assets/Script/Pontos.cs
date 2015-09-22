2c888a1773e685178a7b837293822799f31ef3e5
johnny pinheiro <pinheiro_dom@hotmail.com.br>
2015-09-22 10:37:57 -0300

    implementação menu e game over

---
+++ neve/Assets/Script/Pontos.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pontos : MonoBehaviour {
+
+	public UnityEngine.UI.Text txtDistancia;
+	public static int distancia;
+	
+
+	// Use this for initialization
+	void Start () {
+	distancia = 0;
+		
+		PlayerPrefs.SetInt("Distancia", distancia);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txtDistancia.text = distancia.ToString();
+		distancia ++;
+	}
+}
--- neve/Assets/Script/movePersonagem.cs
+++ neve/Assets/Script/movePersonagem.cs
@@ -6,24 +6,56 @@ public class movePersonagem : MonoBehaviour {
 	public Rigidbody rigd;
 	public float speed;
 	Animator animator;
+	string state;
+	private bool jump;
+	
 
 	// Use this for initialization
 	void Start () {
 	 rigd = GetComponent<Rigidbody>();
 	 animator = GetComponentInChildren<Animator>();
-	 //state = "stop";
+	 state = "stop";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.D)){
-			rigd.velocity = new Vector3(-speed, 0,0);
-			animator.SetBool ("right", true);
-		}else if(Input.GetKey(KeyCode.A)){
-			rigd.velocity = new Vector3(speed, 0,0);
+		movimentation();
+	}
+
+	void movimentation(){
+		if(Input.GetKey(KeyCode.A)){
+			rigd.velocity = new Vector2(speed, rigd.velocity.y);
+				if(!jump){
+					animator.SetBool("left",true);
+				}
+		}else if(Input.GetKey(KeyCode.D)){
+			rigd.velocity = new Vector2(-speed, rigd.velocity.y);
+				if(!jump){
+					animator.SetBool("right",true);
+				}
 		}else{
-			animator.SetBool ("right", false);
+			rigd.velocity = new Vector2(0, rigd.velocity.y);
+			animator.SetBool("left", false);
+			animator.SetBool("right",false);
 		}
 
+
+		if(!jump && Input.GetKeyDown(KeyCode.Space)){
+			rigd.AddForce(Vector2.up*15*60);
+			jump = true;
+			animator.SetBool("jump", true);
+		}
+
+	}
+
+	void OnCollisionEnter(Collision collisionInfo){
+		if(collisionInfo.gameObject.tag == "Ground"){
+			jump = false;
+			animator.SetBool("jump", false);
+			print (jump);
+		}
+		if(collisionInfo.gameObject.tag == "Enemy"){
+			Application.LoadLevel("GameOver");
+		}
 	}
 }
