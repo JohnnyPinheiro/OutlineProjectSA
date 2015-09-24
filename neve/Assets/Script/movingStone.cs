140e17f53f475e949c02e4769722965c0c99ea88
Johnny Pinheiro <pinheiro_dom@hotmail.com.br>
2015-09-24 20:04:09 -0300

    versão alpha
    versãao alpha para entrega

---
+++ neve/Assets/Script/movingStone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingStone : MonoBehaviour {
+
+	public float speed;
+	private float z;
+		
+	void Update () {
+		
+		z = transform.position.z;
+		z += speed * Time.deltaTime;
+		
+		transform.position = new Vector3(transform.position.x,transform.position.y, z);
+		
+		
+		if(z > 1065){
+			Destroy(transform.gameObject);
+			
+		}
+	}
+}
---
+++ neve/Assets/Script/respawPedra.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawPedra : MonoBehaviour {
+	public GameObject obstaclePrefab; //obstacle spawn
+	public float rateSpawn; //temp spawn
+	public float currentTime; //controller time spawn
+	private int position;
+	private float x;
+	public float posi1;
+	public float posi2;
+	public float posi3;
+	
+	// Use this for initialization
+	void Start () {
+		currentTime = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTime += Time.deltaTime;
+		if(currentTime >= rateSpawn){
+			currentTime = 0;
+			position = Random.Range(1,100);
+			if(position < 33){
+				x = posi1;
+			}else if(position >= 33 && position <66){
+				x = posi2;
+			}else{
+				x = posi3;
+			}
+			GameObject tempPrefab = Instantiate(obstaclePrefab) as GameObject;
+			tempPrefab.transform.position = new Vector3(x, transform.position.y,transform.position.z);
+			print (position);
+		}
+	}
+}
