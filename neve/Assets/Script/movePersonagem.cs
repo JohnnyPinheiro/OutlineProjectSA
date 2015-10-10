ef22162801e5299fdcddb2a04206d4b34ff73c18
johnny pinheiro <pinheiro_dom@hotmail.com.br>
2015-10-10 13:17:14 -0300

    novo direcional para mobile com botão de jump, e apk "testealpha02"

--- neve/Assets/Script/movePersonagem.cs
+++ neve/Assets/Script/movePersonagem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class movePersonagem : MonoBehaviour {
 
@@ -8,18 +9,25 @@ public class movePersonagem : MonoBehaviour {
 	Animator animator;
 	string state;
 	private bool jump;
-	
+	public float gravity;
+	public float speedX;
+	private float speedY;
 
 	// Use this for initialization
 	void Start () {
+	 Physics.gravity = new Vector3(0, -gravity, 0);
 	 rigd = GetComponent<Rigidbody>();
 	 animator = GetComponentInChildren<Animator>();
 	 state = "stop";
+	// speedX = 0;
+		speedY = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		movimentation();
+		//MoveCube();
+		MoveJoy();
 	}
 
 	void movimentation(){
@@ -41,11 +49,10 @@ public class movePersonagem : MonoBehaviour {
 
 
 		if(!jump && Input.GetKeyDown(KeyCode.Space)){
-			rigd.AddForce(Vector2.up*15*60);
+			rigd.AddForce(0,15,0, ForceMode.Impulse);
 			jump = true;
 			animator.SetBool("jump", true);
 		}
-
 	}
 
 	void OnCollisionEnter(Collision collisionInfo){
@@ -59,4 +66,47 @@ public class movePersonagem : MonoBehaviour {
 			Application.LoadLevel("GameOver");
 		}
 	}
+
+	void MoveJoy(){
+		if(CrossPlatformInputManager.GetAxis("Horizontal")>0){
+			print("vai direita");
+			rigd.velocity = new Vector2(-speed, rigd.velocity.y);
+				if(!jump){
+					animator.SetBool("right",true);
+				}
+		}else if(CrossPlatformInputManager.GetAxis("Horizontal")<0){
+			print("vai esquerda");
+			rigd.velocity = new Vector2(speed, rigd.velocity.y);
+				if(!jump){
+					animator.SetBool("left",true);
+				}
+		}else{
+			print("parado");
+		}
+	}
+
+	public void jumping(){
+		rigd.AddForce(0,15,0, ForceMode.Impulse);
+		jump = true;
+		animator.SetBool("jump", true);
+	}
+
+
+	/*void MoveCube(){
+       if(Input.GetMouseButton(0)){
+	       if(Input.mousePosition.x < 240){
+	        	//transform.Translate(-cont * Time.deltaTime, 0, 0, Space.World);
+	        	rigd.velocity = new Vector2(speed, rigd.velocity.y);
+	        	if(!jump){
+					animator.SetBool("left",true);
+				}
+	       }else if(Input.mousePosition.x >= 240){
+			    //transform.Translate(cont * Time.deltaTime, 0, 0, Space.World);
+				rigd.velocity = new Vector2(-speed, rigd.velocity.y);
+				if(!jump){
+					animator.SetBool("right",true);
+				}
+			}		
+		}
+	}*/
 }
